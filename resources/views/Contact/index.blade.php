ac2b7fc40a64b183e68dfbbd5f58c6b0e898ee2d
ahmed <ahmedgamalfathy39@gmail.com>
2025-02-16 16:54:26 +0200

    change uri create contactus

--- resources/views/Contact/index.blade.php
+++ resources/views/Contact/index.blade.php
@@ -120,7 +120,7 @@
 
        try {
          loading.style.display = 'block';
-         const response = await fetch('http://127.0.0.1:8000/api/v1/contact-us/create', {
+         const response = await fetch('https://mkani-uae.com/api/v1/contact-us/create', {
            method: 'POST',
            headers: {
              'Content-Type': 'application/json'
