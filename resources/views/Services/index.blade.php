e2e0e147782b8dce77a8a3aa554aba5994fb84f1
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-17 16:26:44 +0200

    updated

--- resources/views/Services/index.blade.php
+++ resources/views/Services/index.blade.php
@@ -7,13 +7,13 @@
         <p class="[font-family:Ping_AR_+_LT] text-3xl md:text-7xl font-bold leading-[40px] md:leading-[86px] text-[#333333]">{{ app()->getLocale() == 'en' ? "What we offer you."  : "ما نقدمة إليك" }} </p>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
             @foreach ($productCategories as $product)
-            <div class="bg-[#fbfcf8] flex justify-start items-stretch flex-col max-w-[389px] grow shrink rounded-xl border-[3px] border-solid border-[#fbfcf8]">
+            <a href='' class="bg-[#fbfcf8] flex justify-start items-stretch flex-col max-w-[389px] grow shrink rounded-xl border-[3px] border-solid border-[#fbfcf8]">
                 <img src="{{ url('storage/'. $product->image) }}" class="h-44 max-w-[initial] object-cover  block border-[none]" />
                 <div class=" flex justify-start flex-col pt-6 pb-[25.5px] px-[23px]">
                   <h2 class="[font-family:Ping_AR_+_LT] text-[29px] font-bold leading-[35px] text-[#333333]">{{ $product->name }}</h2>
                   <p class="[font-family:Ping_AR_+_LT] text-2xl font-normal  leading-[29px] text-[#8e8e8e] self-stretch mt-4">{{ $product->description }}</p>
                 </div>
-              </div>
+            </a>
             @endforeach
         </div>
       </div>
