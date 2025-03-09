a52b1827c8d5289a92625b72f2a62a8101d7bb05
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-03-10 01:51:52 +0200

    t

--- resources/views/Home/Sections/ServiceCategory.blade.php
+++ resources/views/Home/Sections/ServiceCategory.blade.php
@@ -7,7 +7,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
         @foreach ($productCategory as $product)
         <div class="bg-[#fbfcf8] flex justify-start items-stretch flex-col max-w-[389px] grow shrink rounded-xl border-[3px] border-solid border-[#fbfcf8]">
-            <img src="{{ url('storage/'. $product->image) }}" class="h-44 max-w-[initial] object-cover  block border-[none]" />
+            <img src="{{ url('storage/'. $product->image) }}" class="max-w-[initial] object-cover  block border-[none]" />
             <div class=" flex justify-start flex-col pt-6 pb-[25.5px] px-[23px]">
               <h2 class="[font-family:Ping_AR_+_LT] text-[29px] font-bold leading-[35px] text-[#333333]">{{ $product->name }}</h2>
               <p class="[font-family:Ping_AR_+_LT] text-2xl font-normal  leading-[29px] text-[#8e8e8e] self-stretch mt-4">
